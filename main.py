5668a4cd61af14535b442ee5e5aded997fbd79b3
Nikita <gora_mysora@inbox.ru>
2023-10-16 19:55:10 +0300

    1

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
-from PyQt6.QtWidgets import QWidget, QApplication
+from PyQt6.QtWidgets import QWidget, QApplication, QVBoxLayout, QLabel, QPushButton
 class MainWin(QWidget):
     def __init__(self):
         super().__init__()
-kingbar = QApplication()
+        self.set_appear()
+        self.init_UI()
+        self.connects()
+
+    def set_appear(self):
+        self.setWindowTitle("massa")
+        self.resize(1000, 600)
+
+    def init_UI(self):
+        line = QVBoxLayout()
+        label = QLabel('ggg')
+        label1 = QLabel("jjj")
+        baton = QPushButton('press-idiot')
+        line.addWidget(label)
+        line.addWidget(label1)
+        line.addWidget(baton)
+        self.setLayout(line)
+    def connects(self):
+        pass
+
+main = QApplication([])
+window = MainWin()
+window.show()
+main.exec()
